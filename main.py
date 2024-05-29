a4030aada53de269c389680ba25000759eb0bf55
MarcusPBF <Marcus.Miles@pbfenergy.com>
2024-05-29 08:52:35 -0400

    added os module for environment variables

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
+import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret-key-goes-here'
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 
 # CREATE DATABASE
 class Base(DeclarativeBase):
